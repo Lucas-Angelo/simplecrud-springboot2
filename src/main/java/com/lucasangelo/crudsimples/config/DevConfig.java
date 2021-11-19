193c223a9a97554a1545215a07fec1654b914b4f
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-11-18 21:40:06 -0300

    Create dev profile with MySql conector

---
+++ src/main/java/com/lucasangelo/crudsimples/config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.lucasangelo.crudsimples.config;
+
+import com.lucasangelo.crudsimples.services.database.DBService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() {
+
+        if(!this.strategy.equals("create"))
+            return false;
+
+        this.dbService.instantiateTestDatabase();
+        return true;
+    }
+
+}
