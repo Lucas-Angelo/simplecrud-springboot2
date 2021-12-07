53f6983e1625fc9a806e1b1b51394033f6a5e9ac
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-12-07 19:40:56 -0300

    Initial setting of authorization on routes

---
+++ src/main/java/com/lucasangelo/crudsimples/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.lucasangelo.crudsimples.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC_MATCHERS;
+
+    private static final String[] PUBLIC_MATCHERS_GET;
+
+    private static final String[] PUBLIC_MATCHERS_POST;
+
+    static {
+        PUBLIC_MATCHERS = new String[]{
+            "/h2-console/**"
+        };
+        PUBLIC_MATCHERS_GET = new String[] {
+            "/product/**"
+        };
+        PUBLIC_MATCHERS_POST = new String[] {
+            "/user/**"
+        };
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        http.cors().and().csrf().disable();  // Don't save session and Being able to connect from multiple sources
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+	
+    // Being able to connect from multiple sources
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
